382c3e8340bdcb95a4809dce72c4797705e0531d
prabhu <prabhucharan3172@gmail.com>
2023-05-08 14:28:41 +0530

    Ami-ID-Update

--- create.sh
+++ create.sh
@@ -23,7 +23,7 @@ create_ec2() {
 }
 
 #AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g')
-AMI_ID=ami-07ef4d849950da0b3
+AMI_ID=ami-0a4bb3a971637621f
 SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=${SG_NAME} | jq  '.SecurityGroups[].GroupId' | sed -e 's/"//g')
 
 
